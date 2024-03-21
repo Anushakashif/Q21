e64d213c9fc0c87f9599d2def250b39ad09506bf
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 16:16:30 -0700

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,5 @@
+"use strict";
+// 21. They think of something you could store in a Typescript Object. Write a program that creates objects containing
+// these items. 
+let person = { name: "Anusha", fname: "Kashif", age: "19" };
+console.log(person);
---
+++ Q21.ts
@@ -0,0 +1,5 @@
+// 21. They think of something you could store in a Typescript Object. Write a program that creates objects containing
+// these items. 
+
+let person:{name:string, fname:string, age:string} = {name: "Anusha", fname: "Kashif", age: "19"}
+console.log(person)
